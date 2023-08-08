5b0bdb63529babab263ac01af7bf3a4f7296b54b
hantswilliams <hantsawilliams@gmail.com>
2023-08-08 22:53:18 +0000

    this is me updating my code for the 2nd time

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # hha_506_class9
 this is a test repo as a other example 
+
+## Some python example code: 
+```
+variable = 123
+```
---
+++ fake.py
@@ -0,0 +1,12 @@
+## this is fake file 
+
+var1 = 1 
+
+var2 = '2'
+
+var3 = [1, 2, 3]
+
+var4 = {
+    'firstName': 'hants',
+    'lastName': 'williams'
+}
