f904325ca1476cccc909cd27b90e9450adcfcbf0
hantswilliams <hantsawilliams@gmail.com>
2023-08-09 00:47:53 +0000

    uploaded

--- systemChecks.py
+++ systemChecks.py
@@ -1,8 +1,8 @@
 import pandas as pd 
-import sqlalchemy 
 
-dataframe = pd.read_csv('')
+dataframe = pd.read_csv('https://media.githubusercontent.com/media/datablist/sample-csv-files/main/files/customers/customers-100.csv')
 
+dataframe
 
 
 
